f849c5c01404655ae2c7d37cacb10334686fe3fe
desing123788 <93648470+desing123788@users.noreply.github.com>
2021-11-03 19:16:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "lợn hấp babi cưng",
+    introTitle: 'Em bé cute hay bắt nạt anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^Nhấp iuu thương tại đây^^',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Muốn được nắm đôi tay của em và đi cùng em đến cuối đời ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới có 7 tỷ người tại seo lại iuu anh <3',
+    btnReply: 'Hun vào đây <3',
+    reply: 'Yêu anh hấp vì để anh làm culi cả đời cho em ạ <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lợn hấp ạaa 😘😘',
+    messDesc: 'Tối nay 10 rưỡi call iuu thương nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
